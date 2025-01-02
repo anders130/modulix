4a2fe00a2a16fa937fc57a1e7ae7883b3656b990
anders130 <93037023+anders130@users.noreply.github.com>
2025-01-02 17:15:12 +0100

    make lib work with itself

--- lib/default.nix
+++ lib/default.nix
@@ -1,11 +1,12 @@
 inputs: let
     inherit (builtins) attrNames filter listToAttrs map readDir replaceStrings;
+    lib = inputs.nixpkgs.lib // inputs.self.lib;
 in ./.
     |> readDir
     |> attrNames # only get the names of the files
     |> filter (name: name != "default.nix") # skip itself
     |> map (name: {
         name = replaceStrings [".nix"] [""] name;
-        value = import ./${name} inputs;
+        value = import ./${name} {inherit inputs lib;};
     })
     |> listToAttrs
--- lib/mkModules.nix
+++ lib/mkModules.nix
@@ -1,10 +1,8 @@
-inputs: {
+{lib, inputs}: {
     defaultHost ? "default",
     path,
 }: let
     self = inputs.self;
-    lib = self.lib;
-    # mkModule
 in {
 
 }
