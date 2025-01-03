01a114c570ffecfd9eab13aa828bb561c78c631c
anders130 <93037023+anders130@users.noreply.github.com>
2025-01-03 15:11:02 +0100

    remove debug statement

--- lib/mkSymlink.nix
+++ lib/mkSymlink.nix
@@ -13,6 +13,5 @@ in {
     source = path
         |> lib.mkRelativePath self
         |> (p: "${basePath}/${p}")
-        |> (p: lib.debug.traceSeq {inherit p;} p)
         |> hmConfig.lib.file.mkOutOfStoreSymlink;
 }
